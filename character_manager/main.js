5f1d20b66e109d7fc96d608484151b194a28fb37
Jérôme Trips <jerome.trips@tutanota.com>
2022-11-09 11:04:54 +0100

    grabbing characters works

--- character_manager/main.js
+++ character_manager/main.js
@@ -1,23 +1,43 @@
-import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import { setupCounter } from "./counter.js";
+import axios from "axios";
+const body = document.querySelector("body");
+const apiRoot = "https://character-database.becode.xyz";
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="/vite.svg" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+// function error(message) {
+//   console.error(`Error: ${message} `);
+// }
 
-// setupCounter(document.querySelector('#counter'))
+async function getChars() {
+  console.log("now async ....");
+  let response;
+  try {
+    console.log("trying ....");
+    response = await axios.get(apiRoot + "/characters");
+    for (let el of response.data) {
+      console.log(el);
+      let card = document.createElement("div");
+      console.log(card);
+      card.setAttribute("class", "card");
+      let title = document.createTextNode(el.name);
+      console.log(title);
+      //   //let picture = el.image;
+      //   //let desc = el.description;
+      //   //let id = el.id;
+      let h2 = document.createElement("h2");
+      h2.setAttribute("class", "card__title");
+      h2.appendChild(title);
+      console.log(h2);
+      card.appendChild(h2);
+      body.appendChild(card);
+    }
+  } catch (err) {
+    console.log("catch ....");
+    //error(err);
+  }
+}
+
+console.log("getting chars...");
+
+getChars();
+
+//convert to object character
+console.log("END");
