f002dbd59099c39d103faa2671fa3f12f195c39e
Jérôme Trips <jerome.trips@tutanota.com>
2022-11-09 12:17:33 +0100

    starting button updated .gitignore

--- character_manager/main.js
+++ character_manager/main.js
@@ -19,14 +19,26 @@ async function getChars() {
       card.setAttribute("class", "card");
       let title = document.createTextNode(el.name);
       console.log(title);
-      //   //let picture = el.image;
-      //   //let desc = el.description;
-      //   //let id = el.id;
+      //let picture = el.image;
+      let desc = document.createTextNode(el.description);
+      let p = document.createElement("p");
+      p.setAttribute("class", "description");
+      p.appendChild(desc);
+
+      let short = document.createTextNode(el.shortDescription);
+      let h3 = document.createElement("h3");
+      h3.setAttribute("class", "card__short");
+      h3.appendChild(short);
+
       let h2 = document.createElement("h2");
       h2.setAttribute("class", "card__title");
       h2.appendChild(title);
       console.log(h2);
+
       card.appendChild(h2);
+      card.appendChild(h3);
+      card.appendChild(p);
+
       body.appendChild(card);
     }
   } catch (err) {
