06f357699c7830369a100a9a074080ecfe7c7944
Jérôme Trips <jerome.trips@tutanota.com>
2022-11-09 13:31:27 +0100

    implementred button and image

--- character_manager/main.js
+++ character_manager/main.js
@@ -2,9 +2,9 @@ import axios from "axios";
 const body = document.querySelector("body");
 const apiRoot = "https://character-database.becode.xyz";
 
-// function error(message) {
-//   console.error(`Error: ${message} `);
-// }
+function error(message) {
+  console.error(`Error: ${message} `);
+}
 
 async function getChars() {
   console.log("now async ....");
@@ -19,7 +19,7 @@ async function getChars() {
       card.setAttribute("class", "card");
       let title = document.createTextNode(el.name);
       console.log(title);
-      //let picture = el.image;
+      let picture = "data:image/gif;base64," + el.image;
       let desc = document.createTextNode(el.description);
       let p = document.createElement("p");
       p.setAttribute("class", "description");
@@ -27,23 +27,36 @@ async function getChars() {
 
       let short = document.createTextNode(el.shortDescription);
       let h3 = document.createElement("h3");
-      h3.setAttribute("class", "card__short");
+      h3.setAttribute("class", "shortDescription");
       h3.appendChild(short);
 
+      let image = document.createElement("img");
+      image.setAttribute("src", picture);
+      image.setAttribute("class", "profilePicture");
+
       let h2 = document.createElement("h2");
-      h2.setAttribute("class", "card__title");
+      h2.setAttribute("class", "name");
       h2.appendChild(title);
       console.log(h2);
 
+      let btn_more = document.createElement("a");
+      btn_more.setAttribute("class", "btnMore");
+      // TODO create url with id as parameter
+      btn_more.setAttribute("href", "singleCharacter.html");
+      let btn_moreText = document.createTextNode("See Character");
+      btn_more.appendChild(btn_moreText);
+
+      card.appendChild(image);
       card.appendChild(h2);
       card.appendChild(h3);
       card.appendChild(p);
+      card.appendChild(btn_more);
 
       body.appendChild(card);
     }
   } catch (err) {
     console.log("catch ....");
-    //error(err);
+    error(err);
   }
 }
 
