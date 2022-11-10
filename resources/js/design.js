0a04487316f1047b74da4f10ac5de82621b42449
lilouMazzarisi <lisa_mazzarisi@hotmail.com>
2022-11-10 10:47:21 +0100

    card style first design 10.11

---
+++ resources/js/design.js
@@ -0,0 +1,5 @@
+const body = document.querySelector('body');
+body.className =
+    'bg-gradient-to-r from-violet-400 via-pink-400 to-red-400 flex-col items-start';
+const section = document.querySelector('.container');
+section.className = 'flex flex-wrap';
