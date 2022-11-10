8512c5d718c560e219a1a0d409ee2205e5cc5d26
lilouMazzarisi <lisa_mazzarisi@hotmail.com>
2022-11-10 14:22:56 +0100

    changing button in singleChar html

--- resources/js/design.js
+++ resources/js/design.js
@@ -1,5 +1,8 @@
 const body = document.querySelector('body');
 body.className =
-    'bg-gradient-to-r from-violet-400 via-pink-400 to-red-400 flex-col items-start';
+    'p-4 bg-gradient-to-r from-violet-400 via-pink-400 to-red-400 flex-col items-start ';
 const section = document.querySelector('.container');
-section.className = 'flex flex-wrap';
+section.className = 'flex flex-wrap w-4/5 m-auto';
+const addButton = document.querySelector('.btnAdd');
+addButton.className =
+    'w-12 h-12 bg-slate-500 rounded-full pt-2 text-white text-center text-2xl font-bold m-auto';
