5546193c5d1cb719833c3d997d62dc8bab34acbc
Jérôme Trips <jerome.trips@tutanota.com>
2022-11-12 15:02:50 +0100

    added reset button and form prefill on edit page

--- resources/js/addchar.js
+++ resources/js/addchar.js
@@ -4,22 +4,15 @@ import { makeImageSource, error } from './helpers';
 const apiRoot = 'https://character-database.becode.xyz';
 const url = new URL(document.location.href);
 
+const charForm = document.getElementsByClassName('.charForm');
 const pictureBox = document.querySelector('.profilePicture');
-const nameBox = document.querySelector('charname');
+const nameBox = document.querySelector('.charname');
 const shortBox = document.querySelector('.short');
 const longBox = document.querySelector('.description');
 const resetBtn = document.querySelector('.reset');
 
 const charId = url.searchParams.get('id');
 console.log(charId);
-
-function resetInput() {
-    pictureBox.value = null;
-    nameBox.value = null;
-    shortBox.value = null;
-    longBox.value = null;
-    // for some reason this reloads the page with no parameters
-}
 
 async function getEditChar(characterId) {
     let response;
@@ -28,21 +21,17 @@ async function getEditChar(characterId) {
         response = await axios.get(`${apiRoot}/characters/${characterId}`);
         console.log(response);
 
-        const cname = document.createTextNode(response.data.name);
-        const desc = document.createTextNode(response.data.description);
-        const short = document.createTextNode(response.data.shortDescription);
+        nameBox.setAttribute('value', response.data.name);
+        shortBox.setAttribute('value', response.data.shortDescription);
+        longBox.setAttribute('value', response.data.description);
 
-        const image = document.createElement('img');
-        image.setAttribute('src', makeImageSource(response.data.image));
+        pictureBox.setAttribute('src', makeImageSource(response.data.image));
     } catch {
         error('somethin Wong');
     }
 }
 
-resetBtn.addEventListener('click', () => {
-    resetInput();
-});
-
+// // Theres a reset button build into html forms
 switch (charId) {
     case 'newchar':
         break;
